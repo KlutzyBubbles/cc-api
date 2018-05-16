975cdf23a568a0a5f766388f20839a5c98856187
KlutzyBubbles <LTzilantonis@gmail.com>
2018-05-17 09:52:38 +1000

    Semi complete 2.1

--- public/database/config.php
+++ public/database/config.php
@@ -1,10 +1,19 @@
 <?php
 
-define('SERVER', 'localhost');
+define('SERVER', 'ec2-54-204-46-236.compute-1.amazonaws.com');
+define('PORT', '5432');
+define('DATABASE', 'df4r8fjpgm2fip');
+define('USERNAME', 'wrctwpfnyxnczz');
+define('PASSWORD', 'bfc1c84e8c8e80cda534681b4f715f2ab2e74f4da5ad673b67e9c679ce2112f3');
+define('CHARSET', 'UTF8');
+
+/*
+ * define('SERVER', 'localhost');
 define('PORT', '3307');
 define('DATABASE', 'phoenix');
 define('USERNAME', 'root');
 define('PASSWORD', '');
 define('CHARSET', 'UTF8');
+ */
 
 ?>
